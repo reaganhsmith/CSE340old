3baaccac3c761752bb3b34ae07c171399c270d3e
reaganhsmith <reagansmith@byui.edu>
2023-04-19 16:22:21 -0400

    Week one assignment

--- server.js
+++ server.js
@@ -6,13 +6,28 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+
+
+/* ***********************
+ * View Enginen and Templates 
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not all views
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
